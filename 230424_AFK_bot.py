9d6b23fbe863abcc66deda1ccbf21bcdd1d31b51
cinkovic <67762416+cinkovic@users.noreply.github.com>
2023-05-22 19:18:08 +0100

    AFK Bot

---
+++ 230424_AFK_bot.py
@@ -0,0 +1,21 @@
+import pyautogui as pag
+import random
+import time
+
+afk_counter = 0
+curr_coords = pag.position()
+
+while True:
+    # print(curr_coords)
+    if pag.position() == curr_coords:
+        afk_counter += 1
+    else:
+        afk_counter = 0
+        curr_coords = pag.position()
+    if afk_counter > 5:
+        x = random.randint(0, 1920)
+        y = random.randint(0, 1080)
+        pag.moveTo(x, y, duration=0.5)
+        curr_coords = pag.position()
+    print(f'AFK counter: {afk_counter}, current coords: {curr_coords}')
+    time.sleep(2)
