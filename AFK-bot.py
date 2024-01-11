d54a452fa73fbdcc70307cab125ac8f2e0d16ae6
Ubuntu <azureuser@vm2.gbzp4ngcsd1u3facbksg2pci2b.fx.internal.cloudapp.net>
2024-01-11 01:12:43 +0000

    refactored

--- AFK-bot.py
+++ AFK-bot.py
@@ -2,20 +2,27 @@ import pyautogui as pag
 import random
 import time
 
-afk_counter = 0
-curr_coords = pag.position()
+def random_movement():
+    x = random.randint(0, pag.size().width)
+    y = random.randint(0, pag.size().height)
+    pag.moveTo(x, y, duration=random.uniform(0.1, 1.0))
 
-while True:
-    # print(curr_coords)
-    if pag.position() == curr_coords:
-        afk_counter += 1
-    else:
-        afk_counter = 0
-        curr_coords = pag.position()
-    if afk_counter > 6:
-        x = random.randint(0, 1920)
-        y = random.randint(0, 1080)
-        pag.moveTo(x, y, duration=0.5)
-        curr_coords = pag.position()
-    print(f'AFK counter: {afk_counter}, current coords: {curr_coords}')
-    time.sleep(2)
+def main(afk_threshold=6, check_interval=2):
+    afk_counter = 0
+    curr_coords = pag.position()
+
+    while True:
+        if pag.position() == curr_coords:
+            afk_counter += 1
+        else:
+            afk_counter = 0
+            curr_coords = pag.position()
+        
+        if afk_counter > afk_threshold:
+            random_movement()
+            curr_coords = pag.position()
+        
+        time.sleep(random.uniform(1, check_interval))
+
+if __name__ == "__main__":
+    main()
