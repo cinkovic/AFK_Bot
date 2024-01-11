33eabbfd44ed85138850dbfbeecdb3f8fae72bf3
cinkovic <67762416+cinkovic@users.noreply.github.com>
2024-01-11 15:13:46 +0000

    Update AFK-bot.py

--- AFK-bot.py
+++ AFK-bot.py
@@ -2,10 +2,12 @@ import pyautogui as pag
 import random
 import time
 
+
 def random_movement():
     x = random.randint(0, pag.size().width)
     y = random.randint(0, pag.size().height)
     pag.moveTo(x, y, duration=random.uniform(0.1, 1.0))
+
 
 def main(afk_threshold=6, check_interval=2):
     afk_counter = 0
@@ -23,6 +25,7 @@ def main(afk_threshold=6, check_interval=2):
             curr_coords = pag.position()
         
         time.sleep(random.uniform(1, check_interval))
+
 
 if __name__ == "__main__":
     main()
